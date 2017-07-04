ae48cc37f5b7374a272f6858d4ab023a2c20d066
KrishVikram <connect_krishna@hotmail.com>
2017-07-04 12:16:17 +0530

    Add files via upload
    A C++ utility, that if entered a number: 
    1. Says if it is a prime
    2. If not, returns all its factors
    3. And the next closest prime to the entered number.

---
+++ Number_Utils.cpp
@@ -0,0 +1,66 @@
+// Number_Utils.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	unsigned long n;
+	bool bisprime = true;
+	std::vector<int> factors;
+	std::cout << "Enter number:" << endl;
+	cin >> n;
+	if (n == 0 || n == 1) 
+	{ 
+		cout << "Next Biggest prime is: " << 2 <<endl; 
+	}
+	
+	if (n == 2) { cout << "No is Prime by itself"; }
+
+	for (int i = 2; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+		{
+			bisprime = false;
+			factors.push_back(i);
+		}
+	}
+	if (bisprime)
+	{
+		cout << "No is Prime by itself";
+		return 0;
+	}
+	else
+	{
+		cout << "Factors of the given number: " << endl;
+		std::vector<int>::iterator it;
+		for (it = factors.begin(); it != factors.end(); it++) 
+		{
+			cout << *it << " " << endl; 
+		}
+		cout << '\n';
+		cout << "No of Factors: " << factors.size() << endl;
+		while (!bisprime)
+		{
+			n = n + 1;
+			for (int i = 2; i <= n / 2; i++)
+			{
+				if (n % i == 0)
+				{
+					bisprime = false;
+				}
+				else
+				{
+					bisprime = true;
+				}
+			}
+			
+		}
+		cout << "Next Biggest prime is: " << n << endl;
+		return 0;
+	}
+}
+	
